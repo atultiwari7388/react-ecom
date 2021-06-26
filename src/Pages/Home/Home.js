de92618dd1950996d891ce6b6e596d7cfef6994c
Mr. Darling <tiwariatul9526@gmail.com>
2021-06-26 06:19:08 -0400

    adding home items

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,13 @@
+import './Home.css'
+import { headerItems, products } from '../../utils/ProductsData'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div className='item-container'>
+        {headerItems && headerItems.map((item, index) => <p>{item}</p>)}
+      </div>
+    </>
   )
 }
 
