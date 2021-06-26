23f48e097e157d194c1593246a368939afbd29ae
Mr. Darling <tiwariatul9526@gmail.com>
2021-06-26 06:57:35 -0400

    creating slider

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,27 @@
 import './Home.css'
 import { headerItems, products } from '../../utils/ProductsData'
+import Slider from '../../components/Slider/Slider'
+import Banner1 from '../../BannerImages/Banner1.jpg'
+import Banner2 from '../../BannerImages/Banner2.jpg'
+import Banner3 from '../../BannerImages/Banner3.jpg'
+import Banner4 from '../../BannerImages/Banner4.jpg'
+import Banner5 from '../../BannerImages/Banner5.jpg'
+import Banner6 from '../../BannerImages/Banner6.jpg'
 
 const Home = () => {
+  const bannerImage = [Banner1, Banner2, Banner3, Banner4, Banner5, Banner6]
   return (
     <>
       <div className='item-container'>
         {headerItems && headerItems.map((item, index) => <p>{item}</p>)}
+      </div>
+      <div className='home'>
+        <div className='home-container'>
+          {/** Slider */}
+          <Slider images={bannerImage} />
+          <div className='home-row'>{/** products */}</div>
+          <div style={{ marginTop: '40px' }}>{/** Back to top */}</div>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,65 @@
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from '@material-ui/icons'
+import React, { useState, useEffect } from 'react'
+import './Slider.css'
+
+const Slider = ({ images }) => {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const lastIndex = images.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, images])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 5000)
+
+    return () => {
+      clearInterval(slider)
+    }
+  }, [index])
+
+  return (
+    <>
+      <div className='section'>
+        <div className='section-center'>
+          {images.map((image, indexImage) => {
+            let position = 'nextSlide'
+            if (indexImage === index) {
+              position = 'activeSlide'
+            }
+            if (
+              indexImage === index - 1 ||
+              (index === 0 && indexImage === images.length - 1)
+            ) {
+              position = 'lastSlide'
+            }
+            return (
+              <article className={position} key={indexImage}>
+                <img src={image} alt='banner_image' className='banner-img' />
+              </article>
+            )
+          })}
+
+          <p className='prev' onClick={() => setIndex(index - 1)}>
+            <ArrowBackIosOutlined />
+          </p>
+          <p className='next' onClick={() => setIndex(index + 1)}>
+            <ArrowForwardIosOutlined />
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Slider
